a048307f68e5d360e61cbc52736ede479e35b83c
SHHS-Calkins <12359618@c-vusd.org>
2026-02-27 18:03:02 +0000

    Main V0.1

--- thingie.java
+++ thingie.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 public class thingie {
     public static void main(String[] args) {
         boolean exceptionChecker;
+        int num = 0;
+        Boolean oc = false;
+        String name = "";
         Scanner scan = new Scanner(System.in);
         ArrayList<Room> thing = new ArrayList<Room>();
         thing.add(new Room(1, false, "me"));
@@ -14,6 +17,7 @@ public class thingie {
         info.addRoom(5, true, "yes");
         info.addRoom(6, false, "PersonWhoShouldntBeThere");
         System.out.println(info);
+
         System.out.println("Hurry up and give a method: ");
         switch(scan.nextLine()) {
             case "addRoom":
@@ -31,9 +35,54 @@ public class thingie {
                                 System.out.println("Not a number, Stupid.");
                             }
                         } while (!exceptionChecker);
-                        
+                        break;
+                    case 2:
+                        exceptionChecker = false;
+                        do {
+                            try{
+                            System.out.println("Number?");
+                            num = scan.nextInt();
+                            exceptionChecker = true;
+                            } catch(InputMismatchException e) {
+                                scan.next();
+                                System.out.println("Not a number, Stupid.");
+                            }
+                        } while (!exceptionChecker);
+                        exceptionChecker = false;
+                        do {
+                            try{
+                            System.out.println("Number?");
+                            oc = scan.nextBoolean();
+                            exceptionChecker = true;
+                            } catch(InputMismatchException e) {
+                                scan.next();
+                                System.out.println("Not a true/false, try again.");
+                            }
+                        } while (!exceptionChecker);
+                        name = scan.nextLine();
+                        info.addRoom(num, oc, name);
+                        break;
+                    default:
+                        System.out.println("So, like, I said a 1 or a 2, and you put that.");
+                        break;
                         
                 }
+            case "toString":
+                System.out.println(info);
+                break;
+            case "RemoveRoom":
+                System.out.println("Room# to erase from Existence: ");
+                info.RemoveRoom(scan.nextInt());
+                break;
+            case "availableRoomNumbers":
+                System.out.println(info.availableRoomNumbers());
+                break;
+            case "checkIn":
+                System.out.println("Name: ");
+                name = scan.nextLine();
+                System.out.println("Room#: ");
+                num = scan.nextInt();
+                info.checkIn(name, num);
         }
         scan.close();
     }
